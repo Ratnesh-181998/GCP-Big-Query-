8a12996ae9839ab736e332b0bf0b214846410070
Ratnesh Kumar Singh <110965015+Ratnesh-181998@users.noreply.github.com>
2026-07-07 17:12:38 +0530

    Add files via upload

--- transform.js
+++ transform.js
@@ -1,21 +1,7 @@
-function transform(line) {
-    // Split CSV line into an array. Assuming simple CSV without quoted commas.
-    var columns = line.split(',');
-
-    var obj = new Object();
-    // Assuming the order of columns matches the BigQuery Schema
-    obj.order_id = columns[0];
-    obj.product = columns[1];
-    obj.quantity = columns[2];
-    obj.order_status = columns[3];
-    obj.order_date = columns[4];
-
-    // Check if order_status is 'Completed'
-    if (obj.order_status === 'Completed') {
-        // Return a JSON string
+function transform(input) {
+    var obj = JSON.parse(input);
+    if (obj.disease && obj.disease.match(/Cancer$/i)) {
         return JSON.stringify(obj);
     }
-
-    // Return null to filter out the record
     return null;
 }
